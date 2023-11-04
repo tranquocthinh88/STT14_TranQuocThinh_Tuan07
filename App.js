e54ac65437b933cbf08565fb45c482eaf74c0a6b
tranquocthinh88 <97369919+tranquocthinh88@users.noreply.github.com>
2023-11-05 00:39:04 +0700

    update BTTHtuan07

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/HomeScreen';
 import ShopNear from './src/ShopNear';
+import Drinks from './src/Drinks';
+import Orders from './src/Orders';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -9,7 +11,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="ShopNear" component={ShopNear} />
+        <Stack.Screen name="ShopNear" component={ShopNear} options={{headerShown: false}} />
+        <Stack.Screen name="Drinks" component={Drinks} options={{headerShown: false}}/>
+        <Stack.Screen name="Orders" component={Orders} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/ShopNear.js
+++ src/ShopNear.js
@@ -1,58 +1,96 @@
+import { StyleSheet, Text, View, Image,TouchableOpacity,ScrollView, FlatList } from 'react-native';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
-export default function ShopNear() {
-    var [data, setData] = useState();
+import { AntDesign } from "@expo/vector-icons";
+
+export default function ShopNear({navigation}) {
+
+    const [data, setData] = useState();
     useEffect(() => {
-        fetch('https://653f48c19e8bd3be29e02959.mockapi.io/coffeShop')
+        fetch('https://65426f70ad8044116ed35ecf.mockapi.io/shops')
             .then(response => response.json())
             .then(json => { setData(json); })
     }, [])
     console.log(data)
-    return (
-        <View style={styles.container} >
+
+  return (
+    <View style = {styles.container}>
+        <View style={styles.top}>
+            <AntDesign
+            name="arrowleft"
+            size={25}
+            onPress={()=> navigation.goBack()}
+            ></AntDesign>
+            <Text style={{fontSize:30, fontFamily:"arial", fontWeight:700,}} >Shops Near Me</Text>
+            <AntDesign
+            name='search1'
+            size={25}
+            color="#1DD75B"
+            ></AntDesign>
+        </View>
+       <ScrollView>
             <FlatList
                 key='#'
-                data={data}
-                renderItem={({ item }) => (
-                    <View style={styles.box}>
-                        <Image source={{ uri: item.image }} style={styles.img}></Image>
-                        <View>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Image source={item.status ? require('../img/Image 178.png') : require('../img/Image 190.png')} style={{ width: 20, height: 20 }}></Image>
-                                <Text style={{ color: item.status ? 'green' : 'red' }}>{item.status ? 'Accepting Orders' : 'Tempory Unavailable'}</Text>
+                data = {data}
+                renderItem={({item})=>(
+                    <View style={styles.content}>
+                        <TouchableOpacity style={{width:"100%", alignItems:"center", }} onPress={()=>navigation.push('Drinks',{data:item})} >
+                            <Image source={{uri:item.img}} style={{width:"90%", height:100, borderRadius:10}} ></Image>
+                        </TouchableOpacity>
+                        <View style={{flexDirection:"row", justifyContent:"space-between", width:"90%", marginTop:8}}>
+                            <View style={{flexDirection:"row",backgroundColor:"#D9D9D9",width:"50%" , height:30, alignItems:"center", }}>
+                            <Image source={item.status? require('../img/Image 178.png'):require('../img/Image 190.png')} style={{width:20, height:20}} ></Image>
+                            <Text style={{color: item.status? "green":"red", marginLeft:8}} >{item.status ? 'Accepting Orders':'Tempory Unavailable'}</Text>
                             </View>
-                            <View>
-                                <Image source={{ uri: item.image }}></Image>
-                                <Text></Text>
-                                <Image source={{ uri: item.image }}></Image>
+                            <View style={{flexDirection:"row",backgroundColor:"#D9D9D9", width:"40%", height:30, alignItems:"center", justifyContent:"space-evenly"}}>
+                            <Image source={require('../img/Image 180.png')} style={{width:20, height:20}} ></Image>
+                            <Text style={{color:"red"}}>5-10 minutes</Text>
                             </View>
-                            <Text>{item.title}</Text>
-                            <Text>{item.address}</Text>
+                            <Image source={require('../img/Image 181.png')} 
+                            style={{width:20, height:26,alignItems:"center", justifyContent:"space-evenly"}} ></Image>
+                          
                         </View>
+                        <View style={{width:"90%",marginTop:8}}>
+                                <Text style={{fontSize:15, fontFamily:"arial", fontWeight:700,}}>{item.name}</Text>
+                            </View>
+                            <View style={{width:"90%",marginTop:8}}>
+                                <Text style={{fontSize:13, fontFamily:"arial", fontWeight:700,color:"grey"}}>{item.address}</Text>
+                            </View>
                     </View>
+                   
                 )}
-            />
-        </View>
-    )
+
+            ></FlatList>
+       </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    box: {
-        height: 200,
-        width: 350,
-        //backgroundColor: 'red',
-        marginBottom: 20,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10
-    },
-    img: {
-        width: '100%',
-        height: '50%',
-        borderRadius: 10
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: 'space-evenly',
+  },
+  top:{
+    flexDirection:"row",
+   justifyContent:"space-between",
+   marginTop:20,
+   width:"95%"
+
+  },
+  btn:{
+    backgroundColor:"#00BDD6",
+    height:45,
+    borderRadius:8,
+    alignItems:"center",
+    justifyContent:"center"
+  }, content:{
+    alignItems:"center",
+    justifyContent:"space-between",
+    marginTop:20,
+    lineHeight:22,
+    width:400,
+    
+  }
+});
